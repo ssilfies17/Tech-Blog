6efbdf5eb5d23ced53cfc270970f5bcfe66e2144
Sawyer Silfies <ssilfies17@gmail.com>
2022-04-21 23:12:26 -0700

    Login.js logic worked out. Routes changes, login page rendering

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,6 +27,18 @@ router.get("/", withAuth, async (req, res) => {
   }
 });
 
+router.get("/login", (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect("/");
+      return;
+    }
+    res.render("login");
+  });
+  
+  router.get("/dashboard", (req, res) => {
+    res.render("dashboard", { loggedIn: req.session.loggedIn });
+  });
+
 
 
 module.exports = router;
